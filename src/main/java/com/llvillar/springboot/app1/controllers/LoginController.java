ae8f13eff8599082fb48d70f0d44b9bc005f788c
Luis Lopez <llvillar@gmail.com>
2022-11-07 11:10:43 +0100

    login

---
+++ src/main/java/com/llvillar/springboot/app1/controllers/LoginController.java
@@ -0,0 +1,31 @@
+package com.llvillar.springboot.app1.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.llvillar.springboot.app1.model.Usuario;
+
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+    
+    @GetMapping(value = {"/signin"})
+    public String signin(){
+        return "signin";
+    }
+
+    @PostMapping(value = "/login")
+    public String login(Model model, Usuario usuario){
+
+        model.addAttribute("greetings", "¡Bienvenido, " + usuario.getName() + "!");
+        return "welcome";
+    }
+
+    @GetMapping(value = {"/logout"})
+    public String logout(){
+        return "signin";
+    }
+}
---
+++ src/main/java/com/llvillar/springboot/app1/model/Usuario.java
@@ -0,0 +1,58 @@
+package com.llvillar.springboot.app1.model;
+
+public class Usuario {
+
+    private String codigo;
+    private String name;
+    private String password;
+    
+    public Usuario() {
+    }
+    public Usuario(String codigo) {
+        this.codigo = codigo;
+    }
+    public String getCodigo() {
+        return codigo;
+    }
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
+        return result;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Usuario other = (Usuario) obj;
+        if (codigo == null) {
+            if (other.codigo != null)
+                return false;
+        } else if (!codigo.equals(other.codigo))
+            return false;
+        return true;
+    }
+
+    
+    
+}
