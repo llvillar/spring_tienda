3f004df926bbf8601b3511cc58b0643863576a3d
Luis Lopez <llvillar@gmail.com>
2022-11-10 12:47:47 +0100

    messages properties

--- src/main/java/com/llvillar/springboot/app1/controllers/LoginController.java
+++ src/main/java/com/llvillar/springboot/app1/controllers/LoginController.java
@@ -1,5 +1,10 @@
 package com.llvillar.springboot.app1.controllers;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +16,7 @@ import com.llvillar.springboot.app1.model.Usuario;
 @Controller
 @RequestMapping("/login")
 public class LoginController {
+
     
     @GetMapping(value = {"/signin"})
     public String signin(){
@@ -19,6 +25,7 @@ public class LoginController {
 
     @PostMapping(value = "/login")
     public String login(Model model, Usuario usuario){
+
 
         model.addAttribute("greetings", "¡Bienvenido, " + usuario.getName() + "!");
         return "welcome";
