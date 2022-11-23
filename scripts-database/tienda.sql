13c7300d57546554790ed0287f3c48a92616dc23
Luis Lopez <llvillar@gmail.com>
2022-11-23 12:03:39 +0100

    otro cambio

--- scripts-database/tienda.sql
+++ scripts-database/tienda.sql
@@ -5,3 +5,15 @@ CREATE TABLE `Productos` (
   `precio` float NOT NULL,
   PRIMARY KEY (`codigo`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `Clientes` (
+  `codigo` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(100) NOT NULL,
+  `apellidos` varchar(100) DEFAULT NULL,
+  `direccion` varchar(200) DEFAULT NULL,
+  `email` varchar(100) NOT NULL,
+  `dni` varchar(45) DEFAULT NULL,
+  `vip` tinyint DEFAULT NULL,
+  `telefono` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`codigo`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
