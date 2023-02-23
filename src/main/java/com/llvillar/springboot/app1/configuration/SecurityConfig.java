958ad697a8fade6a984d4f7c111d04e0fa63c837
Luis Lopez <llvillar@gmail.com>
2023-02-23 12:45:50 +0100

    jhjh

--- src/main/java/com/llvillar/springboot/app1/configuration/SecurityConfig.java
+++ src/main/java/com/llvillar/springboot/app1/configuration/SecurityConfig.java
@@ -2,7 +2,9 @@ package com.llvillar.springboot.app1.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -58,7 +60,7 @@ public class SecurityConfig {
         return authProvider;
     }
 
-    // @Bean
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         //http.authenticationProvider(authenticationProvider());
@@ -100,6 +102,16 @@ public class SecurityConfig {
     //             .deleteCookies("JSESSIONID");
     //             //.logoutSuccessHandler(logoutSuccessHandler());
     //     return http.build();
+    // }
+
+
+    // @Bean 
+    // AuthenticationManager authManager(HttpSecurity http) throws Exception{
+    //     return http.getSharedObject(AuthenticationManagerBuilder.class)
+    //         .userDetailsService(users())
+    //         .passwordEncoder(passwordEncoder())
+    //         .and()
+    //         .build();
     // }
 
 }
